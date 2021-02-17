ed923b1d42181129d4daf2379927c735e71ce679
Benjamin Sannholm <benjamin@sannholm.se>
2021-02-17 17:28:57 +0100

    Add autodocstring with sphinx format

--- backend/backend/__main__.py
+++ backend/backend/__main__.py
@@ -3,3 +3,17 @@ print("Hello World")
 if True == True:
 
     print("Hello World")
+
+
+def test(input: float) -> int:
+    """[summary]
+
+    :param input: [description]
+    :type input: float
+    :return: [description]
+    :rtype: int
+    """
+    return input
+
+
+test("hello")
