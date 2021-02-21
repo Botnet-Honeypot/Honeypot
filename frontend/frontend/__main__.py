22b2b1e9f703480f0593bb96ec52f5654f418932
ALX99 <46844683+ALX99@users.noreply.github.com>
2021-02-21 15:04:51 +0100

    Make python treat frontend as a package

--- frontend/frontend/__main__.py
+++ frontend/frontend/__main__.py
@@ -1,8 +1,8 @@
 """Entrypoint for the honeypot frontend"""
 import paramiko
 
-from protocols.ssh import ConnectionManager
+import frontend.protocols.ssh as ssh
 
 key = paramiko.RSAKey(filename="./host.key")
-s = ConnectionManager(host_key=key, port=2222)
+s = ssh.ConnectionManager(host_key=key, port=2222)
 s.listen()
