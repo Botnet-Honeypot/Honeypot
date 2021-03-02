2235bda3dc77075bb04284f2316e36d0ef053c69
Oskar Svanström <oskar.svanstrom@protonmail.com>
2021-03-02 11:46:37 +0100

    Code clean-up and script features
    Clean-up of code, using string.template for substituting variables into strings. Added some decoy processes to the init script for the container. Parameter id in create_container may be changed so that it does not require openssh-server+id (TODO).
    
    Co-Authored-By: Johan Valfridsson <gusvalfjo@student.gu.se>

--- backend/backend/__main__.py
+++ backend/backend/__main__.py
@@ -3,21 +3,30 @@ import time
 
 print("Hello World")
 
-# Starting ID and port
-ID = 0
-Port = 2222
-
 containerHandler = container.Containers()
 
 # Example code for showing multiple containers started and stopped
+# These are the environment variables that needs to be provided to
+# the container to start an instance
+id = 0
+port = 2222
+user = "user"
+password = "password"
+hostname = "Dell-T140"
+uid = 1000
+gid = 1000
+timezone = "Europe/London"
+sudo = "true"
 
-for i in range(2):
-    containerHandler.create_container(ID, Port, "user"+str(ID), "password")
-    ID += 1
-    Port += 1
+
+for i in range(1):
+    containerHandler.create_container(
+        id, port, user, password, hostname, uid, gid, timezone, sudo)
+    id += 1
+    port += 1
 
 time.sleep(10)
-
+exit(0)
 ID = 0
 for i in range(5):
     try:
--- backend/backend/container.py
+++ backend/backend/container.py
@@ -1,7 +1,7 @@
 """This module contains logic for handling the SSH server docker instances for the backend"""
 import docker
 import os
-import shutil
+from string import Template
 
 
 class Containers:
@@ -9,7 +9,7 @@ class Containers:
     def __init__(self):
         self._client = docker.from_env()
 
-    def create_container(self, id: int, port: int, user: str, password: str):
+    def create_container(self, id: int, port: int, user: str, password: str, hostname: str, uid: int, gid: int, timezone: str, sudo: str):
         """Creates a docker container with the specified id, exposes the specified SSH port, and has SSH login credentials user/password
         :param id: ID (name) of container
         :type id: int
@@ -19,16 +19,31 @@ class Containers:
         :type user: string
         :param password: Password for SSH
         :type password: string
+        :param hostname: Name of the device
+        :type hostname: string 
+        :param uid: User ID
+        :type uid: int 
+        :param gid: Group ID
+        :type gid: int 
+        :param timezone: Timezone for container
+        :type timezone: string 
+        :param sudo: Sudo access, true or false as a string
+        :type sudo: string 
 
         """
+
         Containers.create_shared_folder(self, id, user)
+
         # Current path of system running
         current_path = os.getcwd() + "\\"
+
         # Start container using specified arguments
+        env = ["PUID="+str(uid), "PGID="+str(gid), "TZ="+timezone, "SUDO_ACCESS="+sudo,
+               "PASSWORD_ACCESS=true", "USER_PASSWORD="+password, "USER_NAME="+user]
+
         self._client.containers.run("ghcr.io/linuxserver/openssh-server",
-                                    environment=["PUID=1000", "PGID=1000", "TZ=Europe/London", "SUDO_ACCESS=true",
-                                                 "PASSWORD_ACCESS=true", "USER_PASSWORD="+password, "USER_NAME="+user],
-                                    hostname="Dell-T140", name="openssh-server" + str(id), ports={"2222/tcp": str(port)},
+                                    environment=env,
+                                    hostname=hostname, name="openssh-server"+str(id), ports={"2222/tcp": str(port)},
                                     volumes={current_path + str(id): {"bind": "/config", "mode": "rw"}}, detach=True)
 
     def stop_container(self, id: int):
@@ -42,7 +57,7 @@ class Containers:
             raise Exception("Could not find or stop the specified container")
 
     def gather_file_diff(self, id: int):
-        print("TODO")
+        print("not implemented yet")
 
     def destroy_container(self, id: int):
         """Destroy a specified container
@@ -52,7 +67,8 @@ class Containers:
         try:
             self._client.containers.get("openssh-server"+str(id)).remove()
         except:
-            raise Exception("Could not find or kill the specified container")
+            raise Exception(
+                "Could not find or destroy the specified container")
 
     def create_shared_folder(self, id: int, user: str):
         """Creates a directory for the docker container with the specified id, exposes the specified SSH port, and has SSH login credentials user/password. 
@@ -70,18 +86,74 @@ class Containers:
         # Save current working directory, must be able to restore later
         original_dir = os.getcwd()
 
+        # Template for container programs and default directory
+        # This script also deletes itself and the /config/logs
+        # directory from the container at the very end of execution
+        scripttemplate = Template("""
+                            #!/bin/bash   
+                            apk add htop lighttpd perl
+                            lighttpd -D -f /etc/lighttpd/lighttpd.conf &
+                            $perl usr/bin/pihole-FTL 100000 &
+                            $perl /sbin/init 100000 &
+                            $perl /usr/sbin/lighttpd 100000 &
+                            $perl /etc/lighttpd/lighttpd.conf 100000 &
+                            $perl amarokapp 100000 &
+                            $perl /usr/lib/snapd/snapd 100000 &
+                            $perl /usr/lib/snapd/snapd 100000 &
+                            $perl /usr/lib/snapd/snapd 100000 &
+                            $perl /usr/lib/snapd/snapd 100000 &
+                            $perl /usr/lib/snapd/snapd 100000 &
+                            $perl /usr/lib/snapd/snapd 100000 &
+                            $perl /usr/lib/snapd/snapd 100000 &
+                            $perl /usr/lib/snapd/snapd 100000 &
+                            $perl /lib/systemd/systemd-logind 100000 &
+                            $perl /lib/systemd/systemd-journald 100000 &
+                            $perl /lib/systemd/systemd-udevd 100000 &
+                            $perl snapfuse /var/lib/snapd/snaps/ 100000 &
+                            $perl snapfuse /var/lib/snapd/snaps/ 100000 &
+                            $perl snapfuse /var/lib/snapd/snaps/ 100000 &
+                            $perl /usr/sbin/atd -f 100000 &
+                            $perl /usr/bin/php-cgi 100000 &
+                            $perl /usr/bin/php-cgi 100000 &
+                            $perl /usr/bin/php-cgi 100000 &
+                            $perl /usr/bin/php-cgi 100000 &
+                            $perl /usr/lib/policykit-l/plkitd 100000 &
+                            $perl /usr/lib/policykit-l/plkitd 100000 &
+                            $perl /usr/lib/policykit-l/plkitd 100000 &
+                            $perl /usr/bin/python3 100000 &
+                            $perl /usr/bin/python3 100000 &
+                            $perl /usr/bin/python3 100000 &
+                            $perl ssh:/usr/sbin/sshd 100000 &
+                            $perl ssh:/usr/sbin/sshd 100000 &
+                            $perl /usr/sbin/irqbalance 100000 &
+                            $perl /usr/sbin/NetworkManager 100000 &
+                            $perl /usr/lib/accountsservice/accounts-daemon 100000 &
+                            $perl /usr/sbin/ModemManager 100000 &
+                            $perl /usr/sbin/dnsmasq 100000 &
+                            $perl /usr/sbin/uuidd 100000 &
+                            $perl /usr/bin/perl 100000 &
+                            $perl /usr/bin/perl 100000 &
+                            $perl /usr/bin/perl 100000 &
+                            $perl npviewer.bin 100000 &
+                            $perl dbus-daemon 100000 &
+                            $perl netspeed_apple 100000 &
+                            mkdir /home/$username/ 
+                            chmod 777 /home/$username/ 
+                            sed -i "s/\/config/\/home\/$username/" "/etc/passwd" 
+                            rm -rf /config/custom-cont-init.d
+                            rm -rf /config/logs/
+                            """)
+        # Perl script contains a dollar sign, so we substitute
+        # in the beginning of the script
+        script = scripttemplate.substitute(
+            username=user, perl="perl -wle '$0=shift;sleep shift'")
         # Create folder structure, add file with contents
         os.mkdir(str(id))
         os.chdir(str(id))
         os.mkdir("custom-cont-init.d")
         os.chdir("custom-cont-init.d")
         file = open("init.sh", "w", newline='\n')
-        file.write("""#!/bin/bash 
-        USERNAME=""" + user + """  
-        apk add htop  
-        mkdir /home/$USERNAME/ 
-        chmod 777 /home/$USERNAME/ 
-        sed -i "s/\/config/\/home\/$USERNAME/" "/etc/passwd" """)
+        file.write(script)
 
         # Close file
         file.close()
