738add4a32eeca515f1d884c2df888c76132678e
Benjamin Sannholm <benjamin@sannholm.se>
2021-03-23 23:30:50 +0100

    WIP gRPC-based HTTP API and server for backend control
    Co-Authored-By: Sophia Pham <sophia_pham@hotmail.com>

--- backend/backend/__main__.py
+++ backend/backend/__main__.py
@@ -1,41 +1,13 @@
-import backend.container as container
-import time
-
-containerHandler = container.Containers()
-
-# Example code for showing multiple containers started and stopped
-# These are the environment variables that needs to be provided to
-# the container to start an instance.
-# Some of these should be given by the frontend of the honeypot.
-id = 0
-port = 2222
-user = "testuser"
-password = "password"
-hostname = "Dell-T140"
-uid = 1000
-gid = 1000
-timezone = "Europe/London"
-sudo = "true"
+import logging
+import backend.http_server as server
 
 
 def main():
-    for i in range(5):
-        containerHandler.create_container(
-            id, port, user, password, hostname, uid, gid, timezone, sudo)
-        id += 1
-        port += 1
+    # Configure logging
+    logging.basicConfig(level=logging.INFO)
 
-    time.sleep(60)
-
-    # Close and destroy containers after 60 seconds
-    ID = 0
-    for i in range(5):
-        try:
-            containerHandler.stop_container(ID)
-            containerHandler.destroy_container(ID)
-        except:
-            print("Could not find or stop the specified container, continuing anyways")
-        ID += 1
+    # Run HTTP server
+    server.start_http_server()
 
 
 if __name__ == '__main__':
