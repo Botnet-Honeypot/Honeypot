9a433a1c2d96ad9443d5f9297b8ab11564f59dc6
Benjamin Sannholm <benjamin@sannholm.se>
2021-04-01 00:26:17 +0200

    Bind backend HTTP server to all interfaces by default

--- backend/backend/config.py
+++ backend/backend/config.py
@@ -5,4 +5,4 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
 
-HTTP_API_BIND_ADDRESS = os.getenv('HTTP_API_BIND_ADDRESS', 'localhost:80')
+HTTP_API_BIND_ADDRESS = os.getenv('HTTP_API_BIND_ADDRESS', '0.0.0.0:80')
