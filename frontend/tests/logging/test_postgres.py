5833222b42d322365ce204bd24ac179011918274
Alex <46844683+ALX99@users.noreply.github.com>
2021-04-05 13:31:01 +0200

    Update frontend/tests/logging/test_postgres.py
    Co-authored-by: Benjamin Sannholm <Sannholm@users.noreply.github.com>

--- frontend/tests/logging/test_postgres.py
+++ frontend/tests/logging/test_postgres.py
@@ -22,7 +22,7 @@ def test_no_runtime_errors():
     session.log_ssh_channel_output(memoryview(b'output on second channel\n'), 2)
     session.log_env_request(1, "TERM", "screen")
     session.log_env_request(1523, "tnsrieotena", "pfeiabpu")
-    session.log_direct_tcpip_request(4, IPv4Address("23.4.42.53"), 80, "google.com", 80)
+    session.log_direct_tcpip_request(4, ip_address("23.4.42.53"), 80, "google.com", 80)
     session.log_x11_request(4, False, "idk", memoryview(b"43"), 1)
     session.log_x11_request(5, True, "idkatall", memoryview(b"434"), 1)
     session.log_port_forward_request("54.123.64.3", 80)
